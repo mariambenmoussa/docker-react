17511a8b0da135e29ce7d4f5dcc25f216a4d9ded
mariambenmoussa <benmoussa.mariam@gmail.com>
2020-12-03 23:19:53 +0100

    change app message

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Good Morning There! 
+          I wad changed on feature branch!  
         </p>
       </div>
     );
